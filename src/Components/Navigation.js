64c123fb78de462eeabe12664b5c81bf730caa30
spidey <mohit.sood@renewbuy.com>
2020-10-03 15:02:30 +0530

    added menu functionality

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Navigation() {
+
+    const [showMenu, setShowMenu] = useState(false)
+
+    let menu
+
+    let menuMask
+
+    if (showMenu) {
+        menu = <div className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow">
+            The Menu
+        </div>
+
+        menuMask = <div
+            className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+            onClick={() => setShowMenu(false)}
+            >
+
+        </div>
+    }
+
     return (
         <nav>
-            <FontAwesomeIcon
+            <span className="text-xl">
+                <FontAwesomeIcon
                 icon={faBars}
+                onClick={() => setShowMenu(!showMenu)}
             />
+            </span>
+
+            {menuMask}            
+
+            {menu}
+
         </nav>
     )
 }
