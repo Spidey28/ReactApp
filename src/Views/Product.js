236848c21860ad45cc27869411b9eab22a118820
spidey <mohit.sood@renewbuy.com>
2020-10-03 20:15:30 +0530

    API response catch error handling done

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios'
 import { useParams } from "react-router-dom";
+import Loader from '../Components/Loader'
 
 
 function Product() {
@@ -9,26 +10,62 @@ function Product() {
     
     const url = `https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/${id}`;
     
-    const [product, setProduct] = useState(null)
+    const [product, setProduct] = useState({
+        loading: false,
+        data: null,
+        error: false
+    })
 
     let content = null
 
     useEffect(() => {
+        setProduct(
+            {
+                loading: true,
+                data: null,
+                error: false
+            }
+        )
+
         axios.get(url)
             .then(response => {
-                setProduct(response.data)
+                setProduct({
+                    loading: false,
+                    data: response.data,
+                    error: false
+                })
+            })
+            .catch(() => {
+                setProduct({
+                    loading: false,
+                    data: null,
+                    error: true
+                })
             })
     }, [url])
 
-    if (product) {
-        content = <div>
-            <h1 className="text-2xl font-bold mb-3">{product.name}</h1>
-            <div>
-                <img src={product.images[0].imageUrl} alt={product.name}/>
-                <div className="font-bold text-xl mb-3">$ {product.price}</div>
-                <div>{product.description}</div>
-            </div>
+    if (product.error) {
+        content = <p> Oops! Please refresh or try again later.</p>
+    }
+
+    if (product.loading) {
+        content = <Loader/>
+    }
+
+    if (product.data) {
+      content = (
+        <div>
+          <h1 className="text-2xl font-bold mb-3">{product.data.name}</h1>
+          <div>
+            <img
+              src={product.data.images[0].imageUrl}
+              alt={product.data.name}
+            />
+            <div className="font-bold text-xl mb-3">$ {product.data.price}</div>
+            <div>{product.data.description}</div>
+          </div>
         </div>
+      );
     }
 
     return (
