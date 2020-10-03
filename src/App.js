78c34fca077b80877c70aa4cd30a033b1b9a927c
spidey <mohit.sood@renewbuy.com>
2020-10-03 16:25:13 +0530

    Implemented views and navigator

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import HelloWorld from './Components/HelloWorld'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import Header from './Components/Header'
 import Footer from "./Components/Footer";
 import './tailwind.css'
+import Home from './Views/Home'
+import About from "./Views/About";
 // import ConterExample from './Components/CounterExample'
 
 function App() {
   return (
     <div>
-      <Header />
-      <HelloWorld name='World!' />
-      <Footer />
+      <Router>
+        <Header />
+
+        <div className="p-3">
+          <Switch>
+
+            <Route exact path="/">
+              <Home />
+            </Route>
+
+            <Route path="/about">
+              <About />
+            </Route>
+
+          </Switch>
+        </div>
+
+        <Footer />
+
+      </Router>
+
       {/* <ConterExample/> */}
     </div>
   );
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { useTransition, animated } from "react-spring";
+import NavigationMenu from './NavigationMenu'
 
 // mask className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
 // menu className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow"
@@ -55,17 +56,11 @@ function Navigation() {
                   className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow p-3"
                   onClick={() => setShowMenu(false)}
                 >
-                  <span className="font-bold">
-                      The Menu
-                  </span>
-                  <ul>
-                      <li>
-                          Home
-                      </li>
-                      <li>
-                          About
-                      </li>
-                  </ul>
+
+                  <NavigationMenu
+                    closeMenu={(() => setShowMenu(false))}
+                  />
+
                 </animated.div>
               )
           )
